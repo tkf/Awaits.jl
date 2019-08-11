eb564cdd4fa0bf2d83a988d94c996a6d9665c17e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-11 02:01:17 +0000

    build based on fec20cb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Awaits.jl-1","page":"Home","title":"Awaits.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Awaits]","category":"page"},{"location":"#Awaits.SyncSeq","page":"Home","title":"Awaits.SyncSeq","text":"SyncSeq([values])\n\nA minimalistic wrapper of Vector{Any} that can be used from multiple threads.\n\n\n\n\n\n","category":"type"}]
+}
